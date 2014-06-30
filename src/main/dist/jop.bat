66fbcb23ab4cd84ff0a8fe4616a5311135e00a92
Francesco Lerro <francesco.lerro@prada.com>
2014-06-30 16:29:56 +0200

    ver. 0.6, renaming to JOP

--- src/main/dist/jop.bat
+++ src/main/dist/jop.bat
@@ -6,8 +6,8 @@ REM More info: http://www.rolandfg.net/2014/06/29/json-commandline-processor/
 REM
 
 SET BD=%~dp0
-SET VER=1.0
-SET NAME=jsonprocessor
+SET VER=0.6
+SET NAME=jop
 SET CP=%BD%\%NAME%-%VER%-all.jar
 
 java -jar "%CP%" %*
