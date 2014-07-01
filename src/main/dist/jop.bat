e311909ff9ec3f20893320881e6a97ceb0b986d9
Francesco Lerro <francesco.lerro@prada.com>
2014-07-01 18:17:10 +0200

    fix handle params with space in bash shell script, renamed output jar

--- src/main/dist/jop.bat
+++ src/main/dist/jop.bat
@@ -1,13 +1,14 @@
 @ECHO OFF
-
 REM
-REM Startup script for jsonprocessor
+REM Startup script for jop, a JSON processor
+REM
 REM More info: http://www.rolandfg.net/2014/06/29/json-commandline-processor/
 REM
 
-SET BD=%~dp0
-SET VER=0.6
 SET NAME=jop
+SET VER=0.6
+
+SET BD=%~dp0
 SET CP=%BD%\%NAME%-%VER%-all.jar
 
 java -jar "%CP%" %*
